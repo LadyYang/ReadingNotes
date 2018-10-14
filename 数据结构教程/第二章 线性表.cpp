afa40055a0da6c657ed34723e919498b1e32f85b
LadyYang <1763615252@qq.com>
2018-10-14 21:58:18 +0800

    add functions

--- 数据结构教程/第二章 线性表.cpp
+++ 数据结构教程/第二章 线性表.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <malloc.h>
 
 /******************* 2.1 线性表及其逻辑结构 ********************
 
@@ -59,7 +60,114 @@ void test2_1() {
 
 /******************** 2.2 线性表的顺序存储结构 ********************
 
+线性表的顺序存储结构是最常用的存储方式，它直接将线性表的逻辑结构映射到存储结构上。
+
+2.2.1 线性表的顺序存储结构--顺序表
+	线性表的顺序存储结构简称为顺序表(sequential list)
+
+2.2.2 顺序表基本运算的实现
+
+
+	
 */
+#define MaxSize 50
+typedef int ElemType;
+
+typedef struct {
+	ElemType data[MaxSize];
+	int length;
+}SqList;
+
+// 1. 建立顺序表
+// 由a中的n个元素建立顺序表
+void CreateList(SqList * &L, ElemType a[], int n) {
+	int i = 0, k = 0;
+
+	L = (SqList*)malloc(sizeof(SqList));
+	while (i < n) {
+		L->data[k] = a[i];
+		k++;
+		i++;
+	}
+
+	L->length = k;
+}
+
+// 2. 顺序表基本运算算法
+// 初始化线性表
+void InitList(SqList* &L) {
+	L = (SqList*)malloc(sizeof(SqList));
+	L->length = 0;
+}
+
+// 销毁线性表
+void DestoryList(SqList* &L) {
+	free(L);
+}
+
+// 判断线性表是否为空表
+bool ListEmpty(SqList* L) {
+	return (L->length == 0);
+}
+
+// 求线性表的长度
+int ListLength(SqList * L) {
+	return (L->length);
+}
+
+// 输出线性表
+void DispList(SqList * L) {
+	for (int i = 0; i < L->length; i++) {
+		printf("%d", L->data[i]);
+	}
+
+	printf("\n");
+}
+
+// 求线性表中的某个数据元素值
+bool GetElem(SqList *L, int i, ElemType & e) {
+	if (i < 1 || i > L->length)
+		return false;
+
+	e = L->data[i - 1];
+	return true;
+}
+
+
+// 按元素值查找
+int LocateElem(SqList * L, ElemType e) {
+	int i = 0;
+
+	while (i < L->length && L->data[i] != e)
+		i++;
+
+	if (i >= L->length)
+		return 0;
+	else
+		return i + 1;
+}
+
+// 插入数据元素
+bool ListInsert(SqList * &L, int i, ElemType e) {
+	int j;
+
+	if (i < 1 || i > L->length + 1)
+		return false;
+
+	i--;
+
+	for (j = L->length; j > i; j--)
+		L->data[j] = L->data[j - 1];
+
+	L->data[i] = e;
+	L->length++;
+
+	return true;
+}
+
+
+
+
 
 void test2_2() {
 	printf("\n 2.2 线性表的顺序存储结构\n");
